5b05b1cc6a01a7e5d9f8719b6222a304e3d43443
phucnguyen050391 <phucnguyen050391@gmail.com>
2025-12-19 00:07:24 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,13 @@
-// Mở Menu Mobile
 function openNav() {
     document.getElementById("mobileNav").style.width = "100%";
 }
-
-// Đóng Menu Mobile
 function closeNav() {
     document.getElementById("mobileNav").style.width = "0%";
 }
-
 $(document).ready(function() {
-    // Tự động tạo mục lục từ thẻ H2
     $('h2').each(function(index) {
         var id = 'section-' + index;
         $(this).attr('id', id);
-        // Thêm vào sidebar phải
         $('#toc-list').append('<a href="#' + id + '" style="display:block; padding:5px 0; color:#333;">' + $(this).text() + '</a>');
     });
 });
